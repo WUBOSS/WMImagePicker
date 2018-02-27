77b92ef06c4a36c8dff3ab61a68f8149f43fbdef
zhanshu <1054258896@qq.com>
2018-02-27 16:55:58 +0800

    Initial Commit

---
+++ WMImagePicker/WMImagePicker.h
@@ -0,0 +1,13 @@
+//
+//  WMImagePicker.h
+//  WMImagePicker
+//
+//  Created by zhanshu on 2018/2/27.
+//  Copyright © 2018年 zhanshu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WMImagePicker : NSObject
+
+@end
---
+++ WMImagePicker/WMImagePicker.m
@@ -0,0 +1,13 @@
+//
+//  WMImagePicker.m
+//  WMImagePicker
+//
+//  Created by zhanshu on 2018/2/27.
+//  Copyright © 2018年 zhanshu. All rights reserved.
+//
+
+#import "WMImagePicker.h"
+
+@implementation WMImagePicker
+
+@end
